45a58d0a37e18d82a70409d7ec377cf6a245b55e
Yura <yuraberezhnyk@gmail.com>
2023-03-08 20:03:05 +0200

    Created project

---
+++ lib/HomePage.dart
@@ -0,0 +1,63 @@
+// ignore_for_file: file_names
+
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  double _textSize = 24;
+  Color _backgroundColor = Colors.white;
+  var i = 1;
+
+  void _changeTextSize() {
+    if (_textSize > 72) {
+      _textSize = 24;
+    } else {
+      setState(() {
+        _textSize += 2;
+      });
+    }
+  }
+
+  void _changeBackgroundColor() {
+    setState(() {
+      _backgroundColor = Color.fromARGB(
+        255,
+        Random().nextInt(256),
+        Random().nextInt(256),
+        Random().nextInt(256),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GestureDetector(
+        onTap: () {
+          _changeBackgroundColor();
+        },
+        child: Container(
+          color: _backgroundColor,
+          child: Center(
+            child: GestureDetector(
+              onTap: () {
+                _changeTextSize();
+              },
+              child: Text(
+                'Hey there',
+                style: TextStyle(fontSize: _textSize),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:testapplication/HomePage.dart';
+
+void main() {
+  runApp(const TestApp());
+}
+
+class TestApp extends StatefulWidget {
+  const TestApp({super.key});
+
+  @override
+  State<TestApp> createState() => _TestAppState();
+}
+
+class _TestAppState extends State<TestApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
